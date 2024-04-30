ecbdb34ef8c9ff6cf5147ffecef26987bba1e61f
il_kimo <vibertikim@yahoo.it>
2024-04-30 16:47:10 +0200

    chore: add descriptions to fugitive shortcuts

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -3,13 +3,9 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    -- Here you can place any configuration commands specific to vim-fugitive,
-    -- though by default it doesn't require any setup to use its basic features.
-    -- For example, setting key bindings:
-    vim.api.nvim_set_keymap('n', '<leader>gts', ':Git<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gta', ':Git add .<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gtc', ':Git commit<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>gtp', ':Git push<CR>', { noremap = true, silent = true })
-    -- Add more key bindings as needed
+    vim.api.nvim_set_keymap('n', '<leader>gts', ':Git status<CR>', { noremap = true, silent = true, desc = 'Git status' })
+    vim.api.nvim_set_keymap('n', '<leader>gta', ':Git add .<CR>', { noremap = true, silent = true, desc = 'Git add .' })
+    vim.api.nvim_set_keymap('n', '<leader>gtc', ':Git commit<CR>', { noremap = true, silent = true, desc = 'Git commit' })
+    vim.api.nvim_set_keymap('n', '<leader>gtp', ':Git push<CR>', { noremap = true, silent = true, desc = 'Git push' })
   end,
 }
