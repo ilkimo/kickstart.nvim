1cff6972cda72fca94f7ce033f1f1e550de5d32d
il_kimo <vibertikim@yahoo.it>
2024-06-04 10:48:55 +0200

    chore(filetree): add auto refresh

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,133 +1,134 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
   config = function()
     require('neo-tree').setup {
       close_if_last_window = true,
-      popup_border_style = "rounded",
+      popup_border_style = 'rounded',
       enable_git_status = true,
       enable_diagnostics = true,
-      enable_normal_mode_for_inputs = false,                             -- Enable normal mode for input dialogs.
-      open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+      use_libuv_file_watcher = true,
+      enable_normal_mode_for_inputs = false, -- Enable normal mode for input dialogs.
+      open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
       sort_case_insensitive = false,
       default_component_configs = {
         container = {
-          enable_character_fade = true
+          enable_character_fade = true,
         },
         indent = {
           indent_size = 4,
           padding = 1, -- extra padding on left hand side
           -- indent guides
           with_markers = true,
-          indent_marker = "│",
-          last_indent_marker = "└──",
-          highlight = "NeoTreeIndentMarker",
+          indent_marker = '│',
+          last_indent_marker = '└──',
+          highlight = 'NeoTreeIndentMarker',
           -- expander config, needed for nesting files
           with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
+          expander_collapsed = '',
+          expander_expanded = '',
+          expander_highlight = 'NeoTreeExpander',
         },
         icon = {
-          folder_closed = "",
-          folder_open = "",
-          folder_empty = "󰜌",
+          folder_closed = '',
+          folder_open = '',
+          folder_empty = '󰜌',
           -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
           -- then these will never be used.
-          default = "*",
-          highlight = "NeoTreeFileIcon"
+          default = '*',
+          highlight = 'NeoTreeFileIcon',
         },
         modified = {
-          symbol = "[+]",
-          highlight = "NeoTreeModified",
+          symbol = '[+]',
+          highlight = 'NeoTreeModified',
         },
         name = {
           trailing_slash = false,
           use_git_status_colors = true,
-          highlight = "NeoTreeFileName",
+          highlight = 'NeoTreeFileName',
         },
         git_status = {
           symbols = {
             -- Change type
-            added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted   = "✖", -- this can only be used in the git_status source
-            renamed   = "󰁕", -- this can only be used in the git_status source
+            added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = '✖', -- this can only be used in the git_status source
+            renamed = '󰁕', -- this can only be used in the git_status source
             -- Status type
-            untracked = "",
-            ignored   = "",
-            unstaged  = "󰄱",
-            staged    = "",
-            conflict  = "",
-          }
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
         },
         commands = {},
         window = {
-          position = "left",
+          position = 'left',
           width = 40,
           mapping_options = {
             noremap = true,
             nowait = true,
           },
           mappings = {
-            ["<space>"] = {
-              "toggle_node",
+            ['<space>'] = {
+              'toggle_node',
               nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
             },
-            ["<2-LeftMouse>"] = "open",
-            ["<cr>"] = "open",
-            ["<esc>"] = "cancel", -- close preview or floating neo-tree window
-            ["P"] = { "toggle_preview", config = { use_float = true } },
-            ["l"] = "focus_preview",
-            ["S"] = "open_split",
-            ["s"] = "open_vsplit",
+            ['<2-LeftMouse>'] = 'open',
+            ['<cr>'] = 'open',
+            ['<esc>'] = 'cancel', -- close preview or floating neo-tree window
+            ['P'] = { 'toggle_preview', config = { use_float = true } },
+            ['l'] = 'focus_preview',
+            ['S'] = 'open_split',
+            ['s'] = 'open_vsplit',
             -- ["S"] = "split_with_window_picker",
             -- ["s"] = "vsplit_with_window_picker",
-            ["t"] = "open_tabnew",
+            ['t'] = 'open_tabnew',
             -- ["<cr>"] = "open_drop",
             -- ["t"] = "open_tab_drop",
-            ["w"] = "open_with_window_picker",
+            ['w'] = 'open_with_window_picker',
             --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-            ["C"] = "close_node",
+            ['C'] = 'close_node',
             -- ['C'] = 'close_all_subnodes',
-            ["z"] = "close_all_nodes",
+            ['z'] = 'close_all_nodes',
             --["Z"] = "expand_all_nodes",
-            ["a"] = {
-              "add",
+            ['a'] = {
+              'add',
               -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
               -- some commands may take optional config options, see `:h neo-tree-mappings` for details
               config = {
-                show_path = "none" -- "none", "relative", "absolute"
-              }
+                show_path = 'none', -- "none", "relative", "absolute"
+              },
             },
-            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-            ["d"] = "delete",
-            ["r"] = "rename",
-            ["y"] = "copy_to_clipboard",
-            ["x"] = "cut_to_clipboard",
-            ["p"] = "paste_from_clipboard",
-            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+            ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+            ['d'] = 'delete',
+            ['r'] = 'rename',
+            ['y'] = 'copy_to_clipboard',
+            ['x'] = 'cut_to_clipboard',
+            ['p'] = 'paste_from_clipboard',
+            ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
             -- ["c"] = {
             --  "copy",
             --  config = {
             --    show_path = "none" -- "none", "relative", "absolute"
             --  }
             --}
-            ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
-            ["q"] = "close_window",
-            ["R"] = "refresh",
-            ["?"] = "show_help",
-            ["<"] = "prev_source",
-            [">"] = "next_source",
-          }
+            ['m'] = 'move', -- takes text input for destination, also accepts the optional config.show_path option like "add".
+            ['q'] = 'close_window',
+            ['R'] = 'refresh',
+            ['?'] = 'show_help',
+            ['<'] = 'prev_source',
+            ['>'] = 'next_source',
+          },
         },
       },
     }
