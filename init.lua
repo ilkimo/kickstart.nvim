d2bdf24140c1d403e51ed9a971308c2f2b7e31c5
il_kimo <vibertikim@yahoo.it>
2024-09-04 22:24:03 +0200

    fix(plugins): import custom plugins properly

--- init.lua
+++ init.lua
@@ -327,15 +327,6 @@ require('lazy').setup({
     end,
   },
 
-  -- BEGIN Kimo add plugins
-  require 'custom.plugins.colorizer',
-  require 'custom.plugins.filetree',
-  require 'custom.plugins.transparent-nvim',
-  require 'custom.plugins.yaml',
-  require 'custom.plugins.fugitive',
-  require 'custom.plugins.csv',
-  -- END Kimo add
-
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -926,7 +917,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
