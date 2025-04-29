303c8fd21086440fd7f6059d80e63bd782cf8c55
il_kimo <vibertikim@yahoo.it>
2025-04-29 16:39:03 +0200

    chore(fugitive): add git diff shortcut

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -4,8 +4,9 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.api.nvim_set_keymap('n', '<leader>gts', ':Git status<CR>', { noremap = true, silent = true, desc = 'Git status' })
-    vim.api.nvim_set_keymap('n', '<leader>gta', ':Git add .<CR>', { noremap = true, silent = true, desc = 'Git add .' })
+    vim.api.nvim_set_keymap('n', '<leader>gta', ':Gwrite<CR>', { noremap = true, silent = true, desc = 'Git add current file' })
     vim.api.nvim_set_keymap('n', '<leader>gtc', ':Git commit<CR>', { noremap = true, silent = true, desc = 'Git commit' })
     vim.api.nvim_set_keymap('n', '<leader>gtp', ':Git push<CR>', { noremap = true, silent = true, desc = 'Git push' })
+    vim.api.nvim_set_keymap('n', '<leader>gtd', ':Gdiffsplit<CR>', { noremap = true, silent = true, desc = 'Git diff split' })
   end,
 }
